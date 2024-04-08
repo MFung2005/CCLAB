4d28357e6f5b49b6d3d59c28952bfca24215a53f
MFung2005 <mxf4929@nyu.edu>
2024-04-08 16:30:22 +0800

    particle world assignment

--- particleworld-template/script.js
+++ particleworld-template/script.js
@@ -1,7 +1,7 @@
 // CCLab Mini Project - 9.R Particles Template
 
-let NUM_OF_PARTICLES = 3; // Decide the initial number of particles.
-
+let NUM_OF_PARTICLES = 400; // Decide the initial number of particles.
+let mySound;
 let particles = [];
 
 function setup() {
@@ -10,8 +10,9 @@ function setup() {
 
   // generate particles
   for (let i = 0; i < NUM_OF_PARTICLES; i++) {
-    particles[i] = new Particle(random(width), random(height));
+    particles[i] = new Particle(random(85,width-195), random(190,height-135));
   }
+  mySound = loadSound("assets/static.mp3");
 }
 
 function draw() {
@@ -31,19 +32,86 @@ class Particle {
     // properties: particle's characteristics
     this.x = startX;
     this.y = startY;
-    this.dia = 30;
+    this.dia = 20;
   }
   // methods (functions): particle's behaviors
   update() {
-    // (add) 
+    // (add)
+    let on = false;
+    let flicker;
+    let press;
+    let buttondist = dist(450,495,mouseX,mouseY);
+    if (buttondist < 12){
+      on = true;
+    } else {
+      on = false;
+    }
+    if (mouseIsPressed){
+      press = true;
+    }
+    if (on == true && mouseIsPressed){
+      flicker = true;
+    }
+    if (flicker == true){
+      push();
+      
+      translate(this.x, this.y);
+      
+      
+      fill(random(0,255));
+      rect(0, 0, this.dia, this.dia);
+      pop();
+      mySound.play();
+    } else {
+      mySound.stop();
+    }
+    if (flicker == false){
+      mySound.stop();
+    }
   }
   display() {
     // particle's appearance
-    push();
-    translate(this.x, this.y);
+    //tv
+    stroke(4);
+    line(350,120,410,160)
+    line(475,120,410,160)
 
-    circle(0, 0, this.dia);
+    noStroke();
+    //bottomtv
+    fill(211);
+    rect(70,height-125,460,20,15);
+    fill(88,57,39);
+    rect(70,height-110,460,20,15);
+    
+    //toptv
+    fill(211);
+    rect(70,175,460,20,15);
+    fill(88,57,39);
+    rect(70,160,460,20,15);
 
-    pop();
+    //tvleftside
+    fill(211)
+    rect(85,180,20,310,15);
+    fill(88,57,39);
+    rect(70,160,20,350,15);
+
+    //tvrightside
+    fill(211)
+    rect(415,180,100,310,15);
+    fill(88,57,39);
+    rect(510,160,20,350,15);
+    ///topbox
+    fill(70);
+    rect(425,195,75,130,5);
+    //bottombox
+    fill(40);
+    rect(425,335,75,140,5);
+
+    //button
+    strokeWeight(2);
+    fill(120);
+    circle(450,495,24);    
+    //circle(0, 0, this.dia);
+
   }
 }
